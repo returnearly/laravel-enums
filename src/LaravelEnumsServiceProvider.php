b8dcf207e1c1aa359bdffa4817fbe8510a2fb91f
Tom Schlick <tom.schlick@lawnstarter.com>
2023-01-29 02:04:35 -0500

    cleanup service provider

--- src/LaravelEnumsServiceProvider.php
+++ src/LaravelEnumsServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace ReturnEarly\LaravelEnums;
 
-use ReturnEarly\LaravelEnums\Commands\LaravelEnumsCommand;
+use ReturnEarly\LaravelEnums\Commands\MakeEnumCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -10,16 +10,8 @@ class LaravelEnumsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-enums')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel-enums_table')
-            ->hasCommand(LaravelEnumsCommand::class);
+            ->hasCommand(MakeEnumCommand::class);
     }
 }
