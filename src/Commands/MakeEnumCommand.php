6640b95d5828643e7055beaf8921c1ae19402177
Tom Schlick <tom.schlick@lawnstarter.com>
2023-01-29 21:32:46 -0500

    make:enum now in command list

--- src/Commands/MakeEnumCommand.php
+++ src/Commands/MakeEnumCommand.php
@@ -2,18 +2,71 @@
 
 namespace ReturnEarly\LaravelEnums\Commands;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\Concerns\CreatesMatchingTest;
+use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Input\InputOption;
 
-class MakeEnumCommand extends Command
+class MakeEnumCommand extends GeneratorCommand
 {
-    public $signature = 'make:enum {name}';
+    use CreatesMatchingTest;
 
-    public $description = 'My command';
+    protected $signature = 'make:enum {name} {--int} {--string}';
 
-    public function handle(): int
+    protected $description = 'Generates an integer or string based enum.';
+
+    protected $type = 'Enum';
+
+    protected function getStub(): string
     {
-        $this->comment('All done');
+        return $this->resolveStubPath("/stubs/enum-{$this->getEnumType()}.stub");
+    }
 
-        return self::SUCCESS;
+    protected function resolveStubPath($stub): string
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+            ? $customPath
+            : __DIR__ . $stub;
+    }
+
+    protected function getDefaultNamespace($rootNamespace): string
+    {
+        return $rootNamespace.'\Enums';
+    }
+
+    protected function qualifyClass($name): string
+    {
+        if (! Str::endsWith($name, 'Enum')) {
+            $name = $name.'Enum';
+        }
+
+        return parent::qualifyClass($name);
+    }
+
+    protected function getEnumType(): string
+    {
+        if ($this->option('int')) {
+            return 'int';
+        }
+
+        if ($this->option('string')) {
+            return 'string';
+        }
+
+        return $this->choice(
+            'What type of enum do you want to create?',
+            [
+                'int',
+                'string',
+            ]
+        );
+    }
+
+    protected function getOptions(): array
+    {
+        return [
+            ['int', null, InputOption::VALUE_NONE, 'Create a integer based enum.'],
+            ['string', null, InputOption::VALUE_NONE, 'Create a string based enum.'],
+        ];
     }
 }
